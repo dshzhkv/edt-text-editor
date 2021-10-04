fdd0cf56a3d4a3d522eab17f5cecc6b4a07095a3
dshzhkv <dashunich.f@gmail.com>
2021-10-04 23:12:26 +0500

    insert

--- editor.py
+++ editor.py
@@ -1,163 +1,99 @@
-
 import curses
 import sys
 import argparse
+from enum import Enum
 
 
 class Window:
     def __init__(self, n_rows, n_cols):
         self.n_rows = n_rows
         self.n_cols = n_cols
-        
+
+
+class CellType(Enum):
+    GAP = 1
 
 
 class Buffer:
-    def __init__(self, lines):
-        self.text = lines
-        self.buffer = [""]*50
+    def __init__(self, cursor):
+        # self.text = lines
+        self.buffer = [CellType.GAP] * 50
         self.gap_size = 10
-        self.gap_left = 0
-        self.gap_right = self.gap_size - self.gap_left - 1
-        self.size = 10 
-
+        self.cursor = cursor
+        self.gap_left_index = self.cursor.pos_x
+        self.gap_right_index = self.gap_left_index + self.gap_size - 1
+        self.size = 10
 
     def __len__(self):
-        return len(self.text)
+        return len(self.buffer)
 
     def __getitem__(self, index):
-        return self.text[index]
+        return self.buffer[index]
 
+    def grow(self, position):
+        a = self.size * [CellType.GAP]
 
-    def grow(self, k, position): 
-        a = self.size*[""]
+        for i in range(position, self.size):
+            a[i - position] = self.buffer[i]
 
-        for i in range(position, self.size): 
-            a[i - position] = self.buffer[i]; 
+        self.buffer += self.size * [CellType.GAP]
 
+        for i in range(self.gap_size):
+            self.buffer[i + position] = CellType.GAP
 
-        for i in range(k): 
-            self.buffer[i + position] = ''; 
- 
+        for i in range(self.size):
+            self.buffer[position + self.gap_size + i] = a[i]
 
-        for i in range(k): 
+        self.size += self.gap_size
+        self.gap_right_index += self.gap_size
 
-            self.buffer[position + k + i] = a[i]; 
+    def left(self, position):
+        while position < self.gap_left_index:
+            self.gap_left_index -= 1
+            self.gap_right_index -= 1
+            self.buffer[self.gap_right_index + 1] = self.buffer[self.gap_left_index]
+            self.buffer[self.gap_left_index] = CellType.GAP
 
-        self.size += k; 
-        self.gap_right += k; 
+    def right(self, position):
+        while position > self.gap_left_index:
+            self.gap_left_index += 1
+            self.gap_right_index += 1
+            self.buffer[self.gap_left_index - 1] = self.buffer[self.gap_right_index]
+            self.buffer[self.gap_right_index] = CellType.GAP
 
- 
-
-    def move_left(self, position): 
-
-        while position < self.gap_left:
-            self.gap_left-=1 
-            self.gap_right-=1
-            self.buffer[self.gap_right + 1] = self.buffer[self.gap_left]; 
-            self.buffer[self.gap_left] = ''
-
-    def right(self, position) :
- 
-      
-        # // Move the gap right character by character 
-        # // and the buffers 
-        while (position > self.gap_left):
-         
-            self.gap_left+=1
-            self.gap_right+=1 
-            self.buffer[self.gap_left - 1] = self.buffer[self.gap_right]; 
-            self.buffer[self.gap_right] = '_'; 
-        
-
-  
-    def move_cursor(self, position): 
-    
-        if position < self.gap_left:
-            self.left(position); 
-        
+    def move_gap(self, position):
+        if position < self.gap_left_index:
+            self.left(position)
         else:
             self.right(position)
-        
-    
-    def insert(self, input,  position):
 
-        inp_len = len(input) 
-        i = 0; 
-    
-        if position != self.gap_left: 
-            self.move_cursor(position) 
+    def insert(self, input, position):
+        inp_len = len(input)
+        i = 0
 
-        while (i < inp_len): 
-        
-             
-            if self.gap_right == self.gap_left: 
-                k = 10; 
-                self.grow(k, position); 
-            
-            self.buffer[self.gap_left] =  input[i]; 
-            self.gap_left+=1 
-            i+=1
-            position+=1
-     
+        if position != self.gap_left_index:
+            self.move_gap(position)
 
-     
-    
+        while i < inp_len:
+            if self.gap_right_index == self.gap_left_index:
+                self.grow(position)
 
-
-    # def insert(self, cursor, string):
-    #     pos_x, pos_y = cursor.pos_x, cursor.pos_y
-    #     current = self.lines.pop(pos_y)
-    #     new = current[:pos_x] + string + current[pos_x:]
-    #     self.lines.insert(pos_y, new)
-
-    # def delete(self, cursor):
-    #     pos_x, pos_y = cursor.pos_x, cursor.pos_y
-    #     if (pos_y, pos_x) < (len(self.lines), len(self[pos_y])):
-    #         current = self.lines.pop(pos_y)
-    #         new = self.make_new_line(current, pos_x, pos_y)
-    #         self.lines.insert(pos_y, new)
-
-    # def make_new_line(self, current, pos_x, pos_y):
-    #     if pos_x < len(current):
-    #         return current[:pos_x] + current[pos_x + 1:]
-    #     if self.lines:
-    #         return current + self.lines.pop(pos_y)
-    #     return current
+            self.buffer[self.gap_left_index] = input[i]
+            self.gap_left_index += 1
+            i += 1
+            position += 1
 
 
 class Cursor:
-    def __init__(self, pos_y, pos_x, buffer):
+    def __init__(self, pos_y, pos_x):
         self.pos_y = pos_y
         self.pos_x = pos_x
-        self.buffer = buffer
-
-    def move_up(self, steps):
-        self.pos_y = self.pos_y - steps if self.pos_y > 0 else self.pos_y
-        self.pos_x = min(self.pos_x, len(self.buffer[self.pos_y]))
-
-    def move_down(self, steps):
-        if self.pos_y == len(self.buffer) - 1:
-            self.pos_x = len(self.buffer[self.pos_y])
-        else:
-            self.pos_y = self.pos_y + steps if self.pos_y < len(
-                self.buffer) - 1 else self.pos_y
-            self.pos_x = min(self.pos_x, len(self.buffer[self.pos_y]))
 
     def move_right(self, steps):
-        if self.pos_x < len(self.buffer[self.pos_y]):
-            self.pos_x += steps
-        else:
-            if self.pos_y < len(self.buffer) - 1:
-                self.pos_y += 1
-                self.pos_x = 0
+        self.pos_x += steps
 
     def move_left(self, steps):
-        if self.pos_x > 0:
-            self.pos_x -= steps
-        else:
-            if self.pos_y > 0:
-                self.pos_y -= 1
-                self.pos_x = len(self.buffer[self.pos_y])
+        self.pos_x -= steps
 
 
 def main(stdscr):
@@ -166,33 +102,41 @@ def main(stdscr):
     args = parser.parse_args()
 
     with open(args.filename) as f:
-        buffer = Buffer(f.read().splitlines())
+        text = f.read()
+
     window = Window(curses.LINES - 1, curses.COLS - 1)
-    cursor = Cursor(0, 0, buffer)
+    cursor = Cursor(0, 0)
+    buffer = Buffer(cursor)
+    buffer.insert(text, cursor.pos_x)
 
     while True:
         stdscr.erase()
-        for y, line in enumerate(buffer[:window.n_rows]):
-            stdscr.addstr(y, 0, line[:window.n_cols])
+
+        pos_x = pos_y = 0
+
+        for i in buffer.buffer:
+            if i != CellType.GAP:
+                stdscr.addstr(pos_y, pos_x, i)
+                pos_x += 1
+
         stdscr.move(cursor.pos_y, cursor.pos_x)
 
         k = stdscr.getkey()
         if k == "q":
             sys.exit(0)
-        elif k == "KEY_UP":
-            cursor.move_up(1)
-        elif k == "KEY_DOWN":
-            cursor.move_down(1)
+        # elif k == "KEY_UP":
+        #     cursor.move_up(1)
+        # elif k == "KEY_DOWN":
+        #     cursor.move_down(1)
         elif k == "KEY_LEFT":
             cursor.move_left(1)
         elif k == "KEY_RIGHT":
             cursor.move_right(1)
-        elif k == "KEY_BACKSPACE":
-            if (cursor.pos_y, cursor.pos_x) > (0, 0):
-                cursor.move_left(1)
-            
+        # elif k == "KEY_BACKSPACE":
+        #     if (cursor.pos_y, cursor.pos_x) > (0, 0):
+        #         cursor.move_left(1)
         else:
-            buffer[cursor.pos_y].insert(k, cursor.pos_x)
+            buffer.insert(k, cursor.pos_x)
             for _ in k:
                 cursor.move_right(1)
 
